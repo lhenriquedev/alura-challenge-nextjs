99a802878d3eb2583525256cc4a303c18463036c
Henrique M. <lhenrique.dev@gmail.com>
2025-10-16 00:26:29 -0300

    feat: add gradient bg

--- src/ui/components/hero.tsx
+++ src/ui/components/hero.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export function Hero() {
   return (
     <section className="flex flex-col items-center justify-center py-6">
-      <div className="flex flex-1 flex-col items-center justify-center gap-6">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] h-[300px] md:w-[700px] md:h-[500px] bg-[url('/gradient.svg')] bg-no-repeat bg-contain bg-center opacity-40 blur-3xl"></div>
+
+      <div className="relative flex flex-1 flex-col items-center justify-center gap-6">
         <Image
           className="w-[180px] h-[180px] sm:w-[224px] sm:h-[224px]"
           src="hero.svg"
